158e6fa2546c50f49c6aee0cf6d79c02a8b73fca
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-06-19 22:02:13 +0700

    setup astro

---
+++ personal-website/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [robotsTxt(), tailwind()]
+});

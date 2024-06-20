63497de88552282cf629800cfed541ed4d25eb97
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-06-20 22:48:06 +0700

    handle header and hero in progress

--- personal-website/astro.config.mjs
+++ personal-website/astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [robotsTxt(), tailwind()]
+  integrations: [
+    robotsTxt(),
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["*"],
+        mdi: ["account"],
+        ion: ["*"],
+        ion: ["contrast"],
+      },
+    }),
+  ],
 });
--- personal-website/tailwind.config.mjs
+++ personal-website/tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    // fontFamily: {
+    //   display: ["Poppins"],
+    //   sans: ["Poppins"],
+    // },
+    fontFamily: {
+      sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      white: "#ffffff",
+      orange: "#FF7F3E",
+      "blue-light": "#80C4E9",
+    },
+    fontSize: {
+      sm: "1.8rem",
+      base: "2.4rem",
+      xl: "2.8rem",
+      "2xl": "4.8rem",
+      "3xl": "7.2rem",
+    },
+  },
+  plugins: [],
+};

c1673bb009e0d2201f3ce028d93ea0ea74203387
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-12-18 23:20:20 +0000

    finish projects and styling for now

--- personal-website/tailwind.config.mjs
+++ personal-website/tailwind.config.mjs
@@ -4,7 +4,18 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        // orange: "#FF7F3E",
+        // "blue-light": "#80C4E9",
+        orange: "#F6995C",
+        "light-orange": "#EF9C66",
+        "dark-blue": "#295F98",
+        "light-blue": "#80C4E9",
+        default: "#41444B",
+      }
+    },
     // fontFamily: {
     //   display: ["Poppins"],
     //   sans: ["Poppins"],
@@ -12,11 +23,16 @@ export default {
     fontFamily: {
       sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
     },
-    colors: {
-      // white: "#ffffff",
-      orange: "#FF7F3E",
-      "blue-light": "#80C4E9",
-    },
+    // colors: {
+    //   white: "#ffffff",
+    //   // orange: "#FF7F3E",
+    //   // "blue-light": "#80C4E9",
+    //   orange: "#F6995C",
+    //   "light-orange": "#EF9C66",
+    //   "dark-blue": "#295F98",
+    //   "light-blue": "#80C4E9",
+    //   default: "#41444B",
+    // },
     fontSize: {
       sm: "1.2rem",
       base: "1.8rem",

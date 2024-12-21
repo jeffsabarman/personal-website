133efda8feaf60c2bca249389911bb87ebe848c1
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-12-21 17:19:49 +0000

    handle contact scroll, email link, and add toggle mode in progress

---
+++ personal-website/src/components/Header/ToggleMode.tsx
@@ -0,0 +1,13 @@
+import { Moon, Sun } from "lucide-react";
+// import {useState} from "react"
+
+interface ToggleModeProps {
+
+}
+
+const ToggleMode: React.FC<ToggleModeProps> = ({}) => {
+    // const [mode, setMode] = useState("")
+    return <Moon size="1.6rem" />
+}
+
+export default ToggleMode
---
+++ personal-website/src/utils/shared.ts
@@ -0,0 +1,3 @@
+export const linkToEmail = "mailto:jeffsabarman@gmail.com"
+export const idContactSection = "contact-section"
+export const idContactMenu = "contact-menu"

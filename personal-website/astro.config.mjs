a90470abbecea2caec19749b7cc0728b223f2459
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-12-29 15:18:55 +0000

    install vercel and remove hover cursor pointer

--- personal-website/astro.config.mjs
+++ personal-website/astro.config.mjs
@@ -5,19 +5,20 @@ import tailwind from "@astrojs/tailwind";
 // import icon from "astro-icon";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    robotsTxt(),
-    tailwind(),
-    // icon({
-    //   include: {
-    //     mdi: ["*"],
-    //     mdi: ["account"],
-    //     ion: ["*"],
-    //     ion: ["contrast"],
-    //   },
-    // }),
+  integrations: [react(), robotsTxt(), tailwind()
+  // icon({
+  //   include: {
+  //     mdi: ["*"],
+  //     mdi: ["account"],
+  //     ion: ["*"],
+  //     ion: ["contrast"],
+  //   },
+  // }),
   ],
+  output: "server",
+  adapter: vercel()
 });

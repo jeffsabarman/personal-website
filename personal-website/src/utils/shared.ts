67cec17d1df8ab7d223e23e0509e537dce4882d9
Jeffrey Triandi <jeffsabarman@gmail.com>
2026-07-17 21:16:43 +0100

    feat(ui): add CV button

--- personal-website/src/utils/shared.ts
+++ personal-website/src/utils/shared.ts
@@ -1,3 +1,4 @@
 export const linkToEmail = "mailto:jeffsabarman@gmail.com"
 export const idContactSection = "contact-section"
 export const idContactMenu = "contact-menu"
+export const linkToCV = "/cv/Jeffrey-Sabarman-CV.pdf"
